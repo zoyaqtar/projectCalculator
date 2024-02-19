b90f4c222ce3399404729d3af4dc17e33a66148c
unknown <Administrator@Newcaed59.MDServer.bvb.edu>
2024-02-19 16:30:53 +0530

    Updated code for square and cube operations

--- Calculate.java
+++ Calculate.java
@@ -3,7 +3,7 @@
 import java.io.*;
  
 // Driver Class
-class addition {
+class addition extends ExtendedCalculator {
     public  float sum(float num1, float num2)
     {
         return num1+num2;
@@ -13,17 +13,25 @@ class addition {
     {
         return num1-num2;
     }
+
+   
      
      
       // main function
     public static void main(String[] args)
     {
         float num1 = 10, num2 = 20, res = 0;
+        float operand_1=25;
         addition ob = new addition();
         res = ob.sum(num1, num2);
         System.out.println(res);
 
+
         res = ob.sub(num1, num2);
         System.out.println(res);
+
+        System.out.println(square(operand_1));
+        System.out.println(cube(operand_1));
+        
     }
 }
---
+++ ExtendedCalculator.java
@@ -0,0 +1,10 @@
+public class ExtendedCalculator {
+    static float square(float operand_1){
+        return(operand_1 *operand_1);
+    }
+
+   static float cube(float operand_1){
+        return(operand_1 * operand_1 * operand_1 );
+    }
+    
+}
